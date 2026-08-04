f05b6dbe53cb19bd827d29f3ddbeec90c5b17458
beyondhoa <bennettconsultants@gmail.com>
2026-08-04 08:37:04 -0400

    fix(pwa): add skipWaiting and clientsClaim for instant SW updates

--- artifacts/beyond-hoa-web/src/sw.ts
+++ artifacts/beyond-hoa-web/src/sw.ts
@@ -1,10 +1,29 @@
 /// <reference lib="webworker" />
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js');
 
+declare const self: ServiceWorkerGlobalScope;
+
 // @ts-ignore
 if (workbox) {
+  // 1. Force the waiting service worker to become the active service worker immediately
   // @ts-ignore
-  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+  workbox.core.skipWaiting();
+  
+  // 2. Force the active service worker to take control of all open clients/tabs
+  // @ts-ignore
+  workbox.core.clientsClaim();
+
+  // @ts-ignore
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
 } else {
   console.log('Workbox could not be loaded.');
 }
+
+// 3. Automatically reload the window when a new worker takes control
+self.addEventListener('controllerchange', () => {
+  self.clients.matchAll({ type: 'window' }).then((clients) => {
+    clients.forEach((client) => {
+      client.navigate(client.url);
+    });
+  });
+});
